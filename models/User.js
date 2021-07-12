954c9f5c69cbe162d6bec34fd47d2cfafdafa598
grant-create <grant.s.wilkie@gmail.com>
2021-07-11 23:17:42 -0700

    added new schema in user.js

--- models/User.js
+++ models/User.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose')
 
+const favoriteSchema = new mongoose.Schema({
+    title: String,
+    hike: String
+})
+
 const UserSchema = new mongoose.Schema({
     name: String,
     password: String,
     email: String,
-    favorites: {
-        type: String
-    }
+    favorites: [favoriteSchema],
+
 }, {
     timestamps: true
 })
